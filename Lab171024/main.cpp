aa84d962b4bed45012f69999b97f1e1f624cdf36
Kuanysh Yabekov <ybkuanysh@iMac.local>
2024-10-17 19:39:45 +0500

    initial commit

---
+++ Lab171024/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+    double x, y;
+    std::cout << "Please enter X and Y coordinates\n";
+    std::cin >> x >> y;
+
+    if ( (pow(x, 2) + pow(y, 2) <= 1) 
+        && (pow(x - 2, 2) + pow(y, 2) <= 4))
+    {
+        std::cout << "True\n";
+    } else {
+        std::cout << "False\n";
+    }
+    
+    
+    (pow(x, 2) + pow(y, 2) <= 1) && (pow(x - 2, 2) + pow(y, 2) <= 4) ? std::cout<<"true\n" : std::cout<<"false\n";
+
+    return 0;
+}
